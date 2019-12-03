95237004b8ead697bc24beda3f860fcc190c34a9
Victor Ichalov <ichalov@gmail.com>
2019-12-04 02:36:47 +0300

    Install n98-magerun as part of post-deployment procedure

--- centos-magento2-nginx.sh
+++ centos-magento2-nginx.sh
@@ -21,8 +21,12 @@ cd $MAGE_ROOT
 
 find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} +
 find var generated vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} +
-chown -R :nginx .
 bin/magento setup:di:compile
+
+curl -L -o n98-magerun.phar https://files.magerun.net/n98-magerun.phar
+chmod +x n98-magerun.phar
+
+chown -R :nginx .
 
 yum -y install policycoreutils-python
 semanage fcontext -a -t httpd_sys_rw_content_t "$MAGE_ROOT/app/etc(/.*)?"
