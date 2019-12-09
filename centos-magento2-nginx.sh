e55f05d0db1cad6b8ba27518732247cafe4f94fd
Victor Ichalov <ichalov@gmail.com>
2019-12-09 16:53:26 +0300

    Try to fix the problem of duplicate upstream "fastcgi_backend". This version creates separate php-fpm pool for every virtual host added and reserves the initial www.conf pool for the main site accessible by IP. (Version for testing.)

--- centos-magento2-nginx.sh
+++ centos-magento2-nginx.sh
@@ -6,9 +6,13 @@
 # 1. Creates target directory and adjusts its permissions
 # 2. Installs n98-magerun2 utility globally
 # 3. Sets selinux access permissions
-# 4. Installs nginx site configuration file for port 80 and restarts nginx
+# 4. Installs nginx site configuration file for port 80 and php-fpm pool for
+#    the new virtual host
 # It's supposed to be used after lightsail-centos-lemp.sh script (and may be
 # executed automatically in some cases).
+# The following command are supposed to be called after magento installation:
+# 1. restorecon -Rv <magento_home>
+# 2. service nginx restart
 # MAGE_ROOT and MAGE_HOSTNAME variables will be taken from first and second
 # command line param or requested to be entered from console if MAGE_ROOT is
 # missing.
@@ -41,9 +45,22 @@ restorecon -Rv "$MAGE_ROOT/"
 
 # only install nginx virtual host if $MAGE_HOSTNAME is set
 if [ ! -z $MAGE_HOSTNAME ]; then
+  # create a separate php-fpm pool for the new site
+
+  # first check if www.conf.0 file exists to ensure lightsail-centos-lemp.sh
+  # already worked
+  if [ -f /etc/php-fpm.d/www.conf.0 ]; then
+    cp /etc/php-fpm.d/www.conf /etc/php-fpm.d/$MAGE_HOSTNAME.conf
+    sed -i "s/\[www\]/[$MAGE_HOSTNAME]/g" /etc/php-fpm.d/$MAGE_HOSTNAME.conf
+    sed -i "s/listen\\s*=\\s*\\/run\\/php-fpm\\/php-fpm.sock/listen=\\/run\\/php-fpm\\/php-fpm-$MAGE_HOSTNAME.sock/g" /etc/php-fpm.d/$MAGE_HOSTNAME.conf
+  fi
+
+  # add nginx virtual host for the new site (but it won't be functional before
+  # magento is installed because it's dependent on nginx.conf.sample file from
+  # magento installation).
   cat > /etc/nginx/conf.d/$MAGE_HOSTNAME.conf << EOT
 upstream fastcgi_backend {
-  server  unix:/run/php-fpm/php-fpm.sock;
+  server  unix:/run/php-fpm/php-fpm-$MAGE_HOSTNAME.sock;
 }
 
 server {
